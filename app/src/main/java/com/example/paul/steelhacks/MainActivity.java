4e30ff0b6ad57fc3ea9498074e8dc814e9f17b58
lhmaynard <lhm765@gmail.com>
2017-02-11 12:10:59 -0500

    title screen

--- app/src/main/java/com/example/paul/steelhacks/MainActivity.java
+++ app/src/main/java/com/example/paul/steelhacks/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.paul.steelhacks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
+
+    public void single(View button){
+        Intent intent = new Intent(this, instructions.class);
+        int mode = 0;
+        intent.putExtra("mode", mode);
+        startActivity(intent);
+    }
+
+    public void multi(View button){
+
+    }
+
+
 }
