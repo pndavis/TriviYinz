74728be11160d340aefacc0eb51786c6659fd825
Ryan Yoder <rvy1@pitt.edu>
2017-02-11 15:45:32 -0500

    Attempting to implement question read-in

--- app/src/main/java/com/example/paul/steelhacks/Game.java
+++ app/src/main/java/com/example/paul/steelhacks/Game.java
@@ -9,11 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Game extends AppCompatActivity {
     int score = 0;
     int time = 0;
     double startTime = -1;
     int categ;
+    QuestionsManager manager = null;
+    Questions question = null;
+    Button option1;
+    Button option2;
+    Button option3;
+    Button option4;
+    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,33 +31,52 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         final Intent intent = getIntent();
         categ = intent.getExtras().getInt("cat");
+        t = (TextView) findViewById(R.id.question);
+        option1 = (Button) findViewById(R.id.choice1);
+        option2 = (Button) findViewById(R.id.choice2);
+        option3 = (Button) findViewById(R.id.choice3);
+        option4 = (Button) findViewById(R.id.choice4);
+
 
         new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    while(true) {
-                        TextView t = (TextView) findViewById(R.id.question);
-                        Button option1 = (Button) findViewById(R.id.choice1);
-                        Button option2 = (Button) findViewById(R.id.choice2);
-                        Button option3 = (Button) findViewById(R.id.choice3);
-                        Button option4 = (Button) findViewById(R.id.choice4);
-
-                        t.setText("Hello losers");
-                        option1.setText("Lenny");
-                        option2.setText("Paul");
-                        option3.setText("Ryan");
-                        option4.setText("Dva");
-
-                        option3.setBackgroundColor(Color.GREEN);
-                    }
+            @Override
+            public void run() {
+                String filename = "";
+                switch(categ){
+                    case 0: filename = "Geography";break;
+                    case 1: filename = "Sports";break;
+                    case 2: filename = "Inventions";break;
+                    case 3: filename = "Misc";break;
                 }
-
-                public void buttonA(View button) {
-                    // if correct answer was this index, increment total
-                    // reload strings
+                try {
+                    manager = new QuestionsManager(filename);
                 }
-            }, 2000);
+                catch(FileNotFoundException fnfe){
+                    System.out.println("ERROR: FILE NOT FOUND");
+                }
+                while(true) {
+                    question = manager.getQuestion();
+                    t.setText(question.getQuestion());
+                    option1.setText(question.getOption1());
+                    option2.setText(question.getOption2());
+                    option3.setText(question.getOption3());
+                    option4.setText(question.getOption4());
+
+                }
+            }
+
+
+        }, 20000);
+    }
+
+    public void buttonA(View button) {
+        // if correct answer was this index, increment total
+        // reload strings
+        if(question.getAnswer() == 0) {
+            option1.setBackgroundColor(Color.GREEN);
+            score ++;
         }
+    }
 
 
 
