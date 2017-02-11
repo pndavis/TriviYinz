52f0ec862fc5d663fe000211ff16125286120af3
lhmaynard <lhm765@gmail.com>
2017-02-11 12:22:42 -0500

    Fixed git

---
+++ app/src/main/java/com/example/paul/steelhacks/Instructions.java
@@ -0,0 +1,10 @@
+package com.example.paul.steelhacks;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenny on 2/11/17.
+ */
+
+public class Instructions extends AppCompatActivity {
+}
