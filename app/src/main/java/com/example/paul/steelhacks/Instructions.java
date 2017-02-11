d5a77c4a87c050bcb437a6335077475d0d703ffe
lhmaynard <lhm765@gmail.com>
2017-02-11 12:31:10 -0500

    created instructions activity

--- app/src/main/java/com/example/paul/steelhacks/Instructions.java
+++ app/src/main/java/com/example/paul/steelhacks/Instructions.java
@@ -1,10 +1,18 @@
 package com.example.paul.steelhacks;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by lenny on 2/11/17.
  */
 
 public class Instructions extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
 }
--- app/src/main/java/com/example/paul/steelhacks/MainActivity.java
+++ app/src/main/java/com/example/paul/steelhacks/MainActivity.java
@@ -4,26 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 
     public void single(View button){
-        Intent intent = new Intent(this, instructions.class);
+        Intent intent = new Intent(this, Instructions.class);
         int mode = 0;
         intent.putExtra("mode", mode);
         startActivity(intent);
     }
 
     public void multi(View button){
-        Intent intent = new Intent(this, instructions.class);
+        Intent intent = new Intent(this, Instructions.class);
         int mode = 1;
         intent.putExtra("mode", mode);
         startActivity(intent);
