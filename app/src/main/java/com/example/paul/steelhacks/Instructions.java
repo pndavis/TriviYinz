4ec2bd199f72579ad32ae4882cf5a8609448d3e0
lhmaynard <lhm765@gmail.com>
2017-02-11 13:15:16 -0500

    fixed instructions activity

--- app/src/main/java/com/example/paul/steelhacks/Instructions.java
+++ app/src/main/java/com/example/paul/steelhacks/Instructions.java
@@ -3,16 +3,13 @@ package com.example.paul.steelhacks;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-/**
- * Created by lenny on 2/11/17.
- */
 
 public class Instructions extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+        Intent intent = getIntent();
     }
 }
